da192450265a154c94425a98802747e7e12bad90
vishpat <vishpat@3d0adcd0-783f-0410-be0d-5dc70631559b>
2007-11-23 21:11:30 +0000

    Basic logging added

--- trunk/youtube/api/__init__.py
+++ trunk/youtube/api/__init__.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='youtube.api.log',
+                    filemode='w')
+
+PLAYLISTS_URI       = "http://gdata.youtube.com/feeds/" +\
+                        "projection/users/%s/playlists"
+
+PROFILE_URI         = "http://gdata.youtube.com/feeds/"+\
+                        "projection/users/%s" 
+
+CONTACTS_URI        = "http://gdata.youtube.com/feeds/" +\
+                        "projection/users/%s/contacts" 
+
+SUBSCRIPTIONS_URI   = "http://gdata.youtube.com/feeds/" +\
+                        "projection/users/%s/subscriptions"
