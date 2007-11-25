7d79f8d0b4840294e8820d3ee8f583578778ac4b
vishpat <vishpat@3d0adcd0-783f-0410-be0d-5dc70631559b>
2007-11-25 18:52:05 +0000

    Youtube filesystem objects added

---
+++ youtube/fs/fsobjects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import youtube.api.protocol.YoutubeVideo
+import youtube.api.protocol.YoutubePlaylist
+import youtube.api.protocol.YoutubeUser
+
+class YoutubeFS_SB:
+    pass
+
+class YoutubeFS_Inode:
+    pass
+
+class YoutubeFS_Dentry:
+    pass
+
+class YoutubeFS_File:
+    pass 
