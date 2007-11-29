4d5bcf787f5d5071402ccbbb42ee00bdba0d2021
vishpat <vishpat@3d0adcd0-783f-0410-be0d-5dc70631559b>
2007-11-29 05:14:56 +0000

    Basic fs functionality implemented

--- youtube/fs/youtubefuse.py
+++ youtube/fs/youtubefuse.py
@@ -3,155 +3,219 @@ __author__      = "Vishal Patil"
 __copyright__   = "Copyright 2007 - 2008, Vishal Patil"
 __license__     = "MIT"
 
-
-import sys
 import logging
-from fuse import Fuse
-from fuse import Stat
-from youtube.api.protocol import YoutubeVideo
-from youtube.api.protocol import YoutubePlaylist
-from youtube.api.protocol import YoutubeUser
 
-class YoutubeFUSEStat(Stat):
-   def __init__(self):
-       self.st_mode = 0444 
-       self.st_ino = 0 
-       self.st_dev = 0
-       self.st_nlink = 1
-       self.st_uid  = os.getuid()
-       self.st_gid  = os.getgid()
-       self.st_size = 0 
-       self.st_atime = 0
-       self.st_mtime = 0
-       self.st_ctime = 0
+import os, sys
+import fcntl
+try:
+    import _find_fuse_parts
+except ImportError:
+    pass
+import fuse
+from fuse import Fuse
+
+if not hasattr(fuse, '__version__'):
+    raise RuntimeError, \
+        "your fuse-py doesn't know of fuse.__version__, probably it's too old."
+
+def flag2mode(flags):
+    md = {os.O_RDONLY: 'r', os.O_WRONLY: 'w', os.O_RDWR: 'w+'}
+    m = md[flags & (os.O_RDONLY | os.O_WRONLY | os.O_RDWR)]
+
+    if flags | os.O_APPEND:
+        m = m.replace('w', 'a', 1)
+
+    return m
 
 class YoutubeFUSE(Fuse):
-    """
-    
-    """
     def __init__(self, *args, **kw):
         Fuse.__init__(self, *args, **kw)
-        self.root = "/"
-        logging.debug('YoutubeFS: Init complete')
+        self.root = '/'
+        logging.debug("YoutubeFUSE init complete")
 
     def getattr(self, path):
+        logging.debug("YoutubeFUSE getattr " + path)
+        return os.lstat("." + path)
+
+    def readlink(self, path):
+        logging.debug("YoutubeFUSE readlink " + path)
+        return os.readlink("." + path)
+
+    def readdir(self, path, offset):
+        logging.debug("YoutubeFUSE readdir " + path)
+        for e in os.listdir("." + path):
+            logging.debug("YoutubeFUSE direntry " + e)   
+            yield fuse.Direntry(e)
+
+    def unlink(self, path):
+        logging.debug("YoutubeFUSE unlink " + path)
+        os.unlink("." + path)
+
+    def rmdir(self, path):
+        logging.debug("YoutubeFUSE rmdir " + path)
+        os.rmdir("." + path)
+
+    def symlink(self, path, path1):
+        logging.debug("YoutubeFUSE symlink " + path\
+            + " " + path1)
+        os.symlink(path, "." + path1)
+
+    def rename(self, path, path1):
+        logging.debug("YoutubeFUSE rename " + path\
+            + " " + path1)
+        os.rename("." + path, "." + path1)
+
+    def link(self, path, path1):
+        logging.debug("YoutubeFUSE link " + path\
+            + " " + path1)
+        os.link("." + path, "." + path1)
+
+    def chmod(self, path, mode):
+        logging.debug("YoutubeFUSE chmod " + path
+            + " " + str(mode))
+        os.chmod("." + path, mode)
+
+    def chown(self, path, user, group):
+        logging.debug("YoutubeFUSE chown " + path\
+            + " " + user + " " + group)
+        os.chown("." + path, user, group)
+
+    def truncate(self, path, len):
+        logging.debug("YoutubeFUSE truncate " +\
+            path + " " + str(len))
+        f = open("." + path, "a")
+        f.truncate(len)
+        f.close()
+
+    def mknod(self, path, mode, dev):
+        logging.debug("YoutubeFUSE mknode" +\
+        path + " " + str(mode) + " " + str(dev))
+        os.mknod("." + path, mode, dev)
+
+    def mkdir(self, path, mode):
+        logging.debug("YoutubeFUSE mkdir " +\
+            path + " " + str(mode)) 
+        os.mkdir("." + path, mode)
+
+    def utime(self, path, times):
+        logging.debug("YoutubeFUSE utime " + path +\
+            " " + str(times))
+        os.utime("." + path, times)
+
+    def access(self, path, mode):
+        logging.debug("YoutubeFUSE access " + path +\
+            " " + str(mode))
+        if not os.access("." + path, mode):
+            return -EACCES
+
+    def statfs(self):
         """
-        - st_mode (protection bits)
-        - st_ino (inode number)
-        - st_dev (device)
-        - st_nlink (number of hard links)
-        - st_uid (user ID of owner)
-        - st_gid (group ID of owner)
-        - st_size (size of file, in bytes)
-        - st_atime (time of most recent access)
-        - st_mtime (time of most recent content modification)
-        - st_ctime (platform dependent; time of most recent metadata change on Unix,
-                    or the time of creation on Windows).
+        Should return an object with statvfs attributes (f_bsize, f_frsize...).
+        Eg., the return value of os.statvfs() is such a thing (since py 2.2).
+        If you are not reusing an existing statvfs object, start with
+        fuse.StatVFS(), and define the attributes.
+
+        To provide usable information (ie., you want sensible df(1)
+        output, you are suggested to specify the following attributes:
+
+            - f_bsize - preferred size of file blocks, in bytes
+            - f_frsize - fundamental size of file blcoks, in bytes
+                [if you have no idea, use the same as blocksize]
+            - f_blocks - total number of blocks in the filesystem
+            - f_bfree - number of free blocks
+            - f_files - total number of file inodes
+            - f_ffree - nunber of free file inodes
         """
+        logging.debug("YoutubeFUSE statfs")
+        return os.statvfs(".")
 
-        logging.debug('YoutubeFS: getattr ' + path)
-        return -errno.ENOSYS
+    def fsinit(self):
+        logging.debug("YoutubeFUSE fsinit")
+        os.chdir(self.root)
 
-    def getdir(self, path):
-        """
-        return: [('file1', 0), ('file2', 0), ... ]
-        """
-        logging.debug('YoutubeFS: getdir ' + path)
-        -errno.ENOSYS
+    class YoutubeFUSEFile(object):
 
-    def mythread ( self ):
-        logging.debug('YoutubeFS: mythread')
-        -errno.ENOSYS
+        def __init__(self, path, flags, *mode):
+            self.file = os.fdopen(os.open("." + path, flags, *mode),
+                                  flag2mode(flags))
+            self.fd = self.file.fileno()
 
-    def chmod ( self, path, mode ):
-        logging.debug('YoutubeFS: chmod ' + path + ' ' + mode)
-        -errno.ENOSYS
+        def read(self, length, offset):
+            self.file.seek(offset)
+            return self.file.read(length)
 
-    def chown ( self, path, uid, gid ):
-        logging.debug('YoutubeFS: chown ' + path + \
-            + ' ' + uid + ' ' + gid)
-        -errno.ENOSYS
+        def write(self, buf, offset):
+            self.file.seek(offset)
+            self.file.write(buf)
+            return len(buf)
 
-    def fsync ( self, path, isFsyncFile ):
-        print '*** fsync', path, isFsyncFile
-        logging.debug('YoutubeFS: fsync' + path)
-        -errno.ENOSYS
+        def release(self, flags):
+            self.file.close()
 
-    def link ( self, targetPath, linkPath ):
-        print '*** link', targetPath, linkPath
-        logging.debug('YoutubeFS: link ' + targetPath + \
-            ' ' + linkPath)
-        -errno.ENOSYS
+        def _fflush(self):
+            if 'w' in self.file.mode or 'a' in self.file.mode:
+                self.file.flush()
 
-    def mkdir ( self, path, mode ):
-        logging.debug('YoutubeFS: mkdir ' + path + ' ' + 
-                mode) 
-        -errno.ENOSYS
+        def fsync(self, isfsyncfile):
+            self._fflush()
+            if isfsyncfile and hasattr(os, 'fdatasync'):
+                os.fdatasync(self.fd)
+            else:
+                os.fsync(self.fd)
 
-    def mknod ( self, path, mode, dev ):
-        logging.debug('YoutubeFS: mknod ' + path + ' ' +\
-                mode + ' ' + dev)
-        -errno.ENOSYS
+        def flush(self):
+            self._fflush()
+            os.close(os.dup(self.fd))
 
-    def open ( self, path, flags ):
-        logging.debug('YoutubeFS: open ' + path + \
-            ' ' + str(flags)) 
-        -errno.ENOSYS
+        def fgetattr(self):
+            return os.fstat(self.fd)
 
-    def read ( self, path, length, offset ):
-        logging.debug('YoutubeFS: read ' + path + ' ' +\
-            str(length) + ' ' + str(offset))
-        -errno.ENOSYS
+        def ftruncate(self, len):
+            self.file.truncate(len)
 
-    def readlink ( self, path ):
-        logging.debug('YoutubeFS: readlink ' + path)
-        -errno.ENOSYS
+        def lock(self, cmd, owner, **kw):
+            # The code here is much rather just a demonstration of the locking
+            # API than something which actually was seen to be useful.
 
-    def release ( self, path, flags ):
-        logging.debug('YoutubeFS: release ' + path + \
-            ' ' + str(flags)) 
-        -errno.ENOSYS
+            # Advisory file locking is pretty messy in Unix, and the Python
+            # interface to this doesn't make it better.
+            # We can't do fcntl(2)/F_GETLK from Python in a platfrom independent
+            # way. The following implementation *might* work under Linux. 
+            #
+            # if cmd == fcntl.F_GETLK:
+            #     import struct
+            # 
+            #     lockdata = struct.pack('hhQQi', kw['l_type'], os.SEEK_SET,
+            #                            kw['l_start'], kw['l_len'], kw['l_pid'])
+            #     ld2 = fcntl.fcntl(self.fd, fcntl.F_GETLK, lockdata)
+            #     flockfields = ('l_type', 'l_whence', 'l_start', 'l_len', 'l_pid')
+            #     uld2 = struct.unpack('hhQQi', ld2)
+            #     res = {}
+            #     for i in xrange(len(uld2)):
+            #          res[flockfields[i]] = uld2[i]
+            #  
+            #     return fuse.Flock(**res)
 
-    def rename ( self, oldPath, newPath ):
-        logging.debug('YoutubeFS: rename ' + oldPath +\
-            ' ' + newPath)
-        -errno.ENOSYS
+            # Convert fcntl-ish lock parameters to Python's weird
+            # lockf(3)/flock(2) medley locking API...
+            op = { fcntl.F_UNLCK : fcntl.LOCK_UN,
+                   fcntl.F_RDLCK : fcntl.LOCK_SH,
+                   fcntl.F_WRLCK : fcntl.LOCK_EX }[kw['l_type']]
+            if cmd == fcntl.F_GETLK:
+                return -EOPNOTSUPP
+            elif cmd == fcntl.F_SETLK:
+                if op != fcntl.LOCK_UN:
+                    op |= fcntl.LOCK_NB
+            elif cmd == fcntl.F_SETLKW:
+                pass
+            else:
+                return -EINVAL
+            fcntl.lockf(self.fd, op, kw['l_start'], kw['l_len'])
 
-    def rmdir ( self, path ):
-        logging.debug('YoutubeFS: rmdir ' + path)
-        -errno.ENOSYS
 
-    def statfs ( self ):
-        logging.debug('YoutubeFS: statfs')
-        -errno.ENOSYS
+    def main(self, *a, **kw):
+        self.file_class = self.YoutubeFUSEFile
+        return Fuse.main(self, *a, **kw)
 
-    def symlink ( self, targetPath, linkPath ):
-        logging.debug('YoutubeFS: symlink ' + targetPath +\
-            ' ' + linkPath)
-        -errno.ENOSYS
 
-    def truncate ( self, path, size ):
-        logging.debug('YoutubeFS: truncate ' + path + \
-            ' ' + str(size))
-        -errno.ENOSYS
 
-    def unlink ( self, path ):
-        logging.debug('YoutubeFS: unlink ' + path)
-        -errno.ENOSYS
-
-    def utime ( self, path, times ):
-        logging.debug('YoutubeFS: utime ' + path + \
-            ' ' + str(times))
-        -errno.ENOSYS
-
-    def write ( self, path, buf, offset ):
-        logging.debug('YoutubeFS: write ' + path + \
-            ' ' + str(buf) + ' ' + str(offset))
-        -errno.ENOSYS
-
-if __name__ == "__main__":
-    if (len(sys.argv) == 2):
-        print sys.argv[0] + " username"
-        sys.exit(1)
-
- 
