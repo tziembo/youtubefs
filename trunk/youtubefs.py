419c88aac27edb54c83e3d0e5a155bd76a835c22
vishpat <vishpat@3d0adcd0-783f-0410-be0d-5dc70631559b>
2007-12-12 01:57:03 +0000

    Youtubefs made more usable

--- trunk/youtubefs.py
+++ trunk/youtubefs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import logging
 import fuse
 import sys
 import os
@@ -8,21 +9,30 @@ from youtube.fs.youtubefuse import YoutubeFUSE
 
 fuse.fuse_python_api = (0, 2)
 
+mountError = \
+"""
+Problem mounting YoutubeFS:
+1) Please make sure the target directory is empty
+2) YoutubeFS is not already installed
+"""
 def helpMessage(program):
     msg = "%s youtube_username mountpoint" % program
     print msg
-    
+   
 if __name__ == "__main__":
-    usage = """
-Userspace nullfs-alike: mirror the filesystem tree from some point on.
 
-""" + Fuse.fusage
+    if len(sys.argv) != 3:
+        helpMessage(sys.argv[0])
+        sys.exit(1)
 
-    server = YoutubeFUSE(version="%prog " + fuse.__version__,
+    try:
+        usage = """youtubefs: youtubefs filesystem""" + Fuse.fusage
+        server = YoutubeFUSE(version="%prog " + fuse.__version__,
                  usage=usage,dash_s_do='setsingle')
-    
-    server.username     = "tanuvishu"
-    server.parse(values=server, errex=1)
-    server.main()
-
+        server.username     = sys.argv[1] 
+        server.parse(values=server, errex=1)
+        server.main()
+    except Exception,inst:
+        logging.critical("YoutubeFS main %s",str(inst))
+        print mountError 
     
